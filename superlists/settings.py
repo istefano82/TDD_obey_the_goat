e96d11348c7941cdb7835d40a78600f073801bb3
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-02-26 22:38:06 +0200

    spiked in custom passwordless auth backend

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ivaylo.stefanov82@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
@@ -38,6 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend', 
 ]
 
 MIDDLEWARE = [
@@ -51,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'superlists.urls'
+
+
 
 TEMPLATES = [
     {
@@ -81,6 +96,22 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+    'console': {
+        'level': 'DEBUG',
+        'class': 'logging.StreamHandler',
+    },
+},
+'loggers': {
+     'django': {
+        'handlers': ['console'],
+     },
+  },
+'root': {'level': 'INFO'},
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
