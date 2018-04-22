ba926ffaf524e45c08becd7a46d4655c11617127
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-04-22 17:55:34 +0300

    Create preauthenticate sessions remotely

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from unittest import skip
+from .server_tools import reset_database
 import os
 import time
 
@@ -13,9 +14,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
